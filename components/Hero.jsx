f707702d0d58b449627d14dc59eb31fbe39113f1
shakilshagu <muhammadshakil1968@gmail.com>
2024-07-21 19:27:34 +0530

    Hero section Component done

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-   <div>
-    
-   </div>
-  )
-}
+    <div className="flex   md:h-[700px] w-full ">
+      <div className="flex w-full px-[150px]  py-[55px] h-[480]">
+        <div className="flex flex-col justify-center items-center pl-10">
+          <h1 className="text-[40px] font-bold mb-2">
+            Your Best Value Proposition{" "}
+          </h1>
+          <p className="text-gray-700 mt-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <button className="p-4 border rounded-md mt-5">
+            Start free trils{" "}
+          </button>
+        </div>
+        <div className="md:w-full">
+          <img
+            src="https://wallpapers-clan.com/wp-content/uploads/2023/11/zelda-link-with-sword-art-desktop-wallpaper-preview.jpg"
+            alt="Card image"
+            className="w-full h-full object-cover rounded-l-lg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
