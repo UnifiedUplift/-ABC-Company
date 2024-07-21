1897d25756eb90de966c37c97e0ed0b5e1352769
shakilshagu <muhammadshakil1968@gmail.com>
2024-07-21 20:03:37 +0530

    added New Section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,28 +2,20 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="flex   md:h-[700px] w-full ">
-      <div className="flex md:w-full  px-[150px]  py-[55px] h-[480]">
-        <div className="flex flex-col justify-center items-start pl-10">
-          <h1 className="text-[40px] font-bold mb-2">
-            Your Best Value Proposition{" "}
-          </h1>
-          <p className="text-gray-700 mt-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-          <button className="p-3 items-start border rounded-md mt-5">
-            Start free trils{" "}
-          </button>
+    <div className="container flex flex-col px-6 py-4 mx-auto space-y-6 md:h-128 md:py-16 md:flex-row md:items-center md:space-x-6">
+        <div className="flex flex-col items-center w-full md:flex-row md:w-1/2">
+            <div className="max-w-lg md:mx-12 md:order-2">
+                <h1 className="text-3xl font-medium tracking-wide text-gray-800 dark:text-white md:text-4xl">The best Apple Watch apps</h1>
+                <p className="mt-4 text-gray-600 dark:text-gray-300">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut quia asperiores alias vero magnam recusandae adipisci ad vitae laudantium quod rem voluptatem eos accusantium cumque.</p>
+                <div className="mt-6">
+                    <a href="#" className="block px-3 py-2 font-semibold text-center text-white transition-colors duration-200 transform border rounded-md md:inline ">Download from App Store</a>
+                </div>
+            </div>
         </div>
-        <div className="md:w-[80%] w-[80%]">
-          <img
-            src="https://wallpapers-clan.com/wp-content/uploads/2023/11/zelda-link-with-sword-art-desktop-wallpaper-preview.jpg"
-            alt="Card image"
-            className="w-full h-full object-cover rounded-l-lg"
-          />
+
+        <div className="flex items-center justify-center w-full h-96 md:w-1/2">
+            <img className="object-cover w-full h-full max-w-2xl rounded-md" src="https://wallpapers-clan.com/wp-content/uploads/2023/11/zelda-link-with-sword-art-desktop-wallpaper-preview.jpg" alt="apple watch photo"/>
         </div>
-      </div>
     </div>
   );
 };
